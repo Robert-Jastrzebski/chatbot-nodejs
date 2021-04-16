913a6084acf4c40db9d5327bf7b844ffbfc47080
robert <partnerprojektu@gmail.com>
2021-04-16 14:15:33 +0200

    fix render

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.get("/", (req, res) => {
   res.render("index"); 
 });
 
-app.get("/", (req, res) => {
-  res.render("script");
-});
+
 
 axios.get('https://restcountries.eu/rest/v2/name/poland')
 .then((response) => {
