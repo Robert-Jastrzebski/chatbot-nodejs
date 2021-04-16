f7cc694939f9afbc4aa2a7e0a53f5422518bf0be
robert <partnerprojektu@gmail.com>
2021-04-16 20:04:48 +0200

    changes in html

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const express = require("express");
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const path = require("path");
 const app = express();
-const axios = require("axios")
+const axios = require("axios");
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index"); 
+  res.render("index");
 });
 
 
 
 
 
-axios.get('https://restcountries.eu/rest/v2/name/poland')
-.then((response) => {
-  console.log(response.data)
-})
-.catch(console.error)
-
-
-
-
 app.listen(port, () => {
-    console.log("server started on port 3000");
+  console.log("server started on port 3000");
 });
