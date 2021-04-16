6c52cce30df5f587815441ce5c98b517e6e48359
robert <partnerprojektu@gmail.com>
2021-04-16 20:23:58 +0200

    last corrects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const path = require("path");
 const app = express();
-const axios = require("axios");
+
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
