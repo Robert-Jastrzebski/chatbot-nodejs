6418c5692e339ff117bd449871c4fdfc217af81c
robert <partnerprojektu@gmail.com>
2021-04-17 13:19:59 +0200

    add api

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const port = process.env.PORT || 3000;
 const path = require("path");
 const app = express();
+const axios = require("axios");
 
+// body parsed middleware express
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,7 +19,33 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.post("/sendMessage", (req, res) => {
+  // req.body.message - zmienna z wiadomoscia z czatu od uzytkownika
+  // https://restcountries.eu/rest/v2/name/{name}
+  // docs: #
+  const url = `https://restcountries.eu/rest/v2/name/${req.body.message}`;
+  console.log(url);
+  axios
+    .get(url)
+    .then((countryInfo) => {
+      let responseData = {};
+      let success = true;
+      try {
+        responseData = {
+          country: countryInfo.data[0].name,
+          currency: countryInfo.data[0].currencies[0].name,
+        };
+      } catch (e) {
+        success = false;
+      }
 
+      res.json({
+        success: success,
+        data: responseData,
+      });
+    })
+    .catch((err) => res.json({ success: false }));
+});
 
 
 
